b3f560918fe1cfa5df2f6a42084c43113f998230
Andrei Alexandru Slatinaru (Andy) <53469676+AndyAND-Y@users.noreply.github.com>
2024-02-19 15:19:10 +0200

    Update route.ts

--- src/app/api/keep-alive/route.ts
+++ src/app/api/keep-alive/route.ts
@@ -1,6 +1,8 @@
 import prisma from '@/lib/prismadb';
 import { NextResponse } from 'next/server';
 
+export const revalidate = 0;
+
 export async function GET(request: Request) {
 
     const numberLinks = await prisma.link.count();
